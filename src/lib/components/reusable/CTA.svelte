4e09e8e445e800df15aefe8a644335fc8d0b2e88
nico <nico.03727+github@gmail.com>
2023-11-01 17:28:55 -0400

    feat: replace cta stuff

--- src/lib/components/reusable/CTA.svelte
+++ src/lib/components/reusable/CTA.svelte
@@ -4,8 +4,8 @@
 
 <div class="hidden md:flex flex-row p-8 m-4 bg-muted rounded-xl justify-between items-center">
 	<div>
-		<h1>Test</h1>
-		<p>Description of why you should sign up because we are awesome.</p>
+		<h1>Start focusing on building your app</h1>
+		<p>Leave the log collection to us.</p>
 	</div>
 	<div>
 		<Button>Get Started</Button>
